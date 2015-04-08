51972605bf1ddf7d441292a5b6e78507785a36b3
RanjanD <ranjid.93@gmail.com>
2015-04-08 12:54:38 +0530

    Initial Commit

---
+++ GetAww/GetAww/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GetAww
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WebClient client = new WebClient();
+            //client.DownloadFile(@"http://i.imgur.com/rt1xXSe.jpg", @"C:\Temp\test.jpeg");
+            //client.DownloadFile(@"http://www.reddit.com/r/wallpapers/", @"C:\\Temp\tesePage.html");
+            WallpaperGetter wallpaperGetter;
+            try
+            {
+                wallpaperGetter = new WallpaperGetter("wallpapers");
+                wallpaperGetter.GetWallpapers();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong!");
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ GetAww/GetAww/WallpaperGetter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedditSharp;
+using System.IO;
+using System.Net;
+
+namespace GetAww
+{
+    class WallpaperGetter
+    {
+        private string path, subRedditName;
+        Reddit myReddit;
+        WebClient client;
+        DirectoryInfo wallpaperDirectory;
+        private int successfulDownloadCount;
+
+        public WallpaperGetter(string subRedditName)
+        {
+            path = @"C:\Wallpapers\";
+            myReddit = new Reddit();
+            client = new WebClient();
+            successfulDownloadCount = 0;
+            this.subRedditName = @"/r/"+subRedditName;
+        }
+
+        public void GetWallpapers()
+        {
+            var subReddit = myReddit.GetSubreddit(subRedditName);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+
+            }
+            wallpaperDirectory = new DirectoryInfo(path);
+            foreach (FileInfo oldPicture in wallpaperDirectory.GetFiles())
+            {
+                oldPicture.Delete();
+            }
+            if(subReddit != null)
+            {
+                //try
+                //{
+                    var top30 = subReddit.Hot.Take(30);
+                    foreach (var post in top30)
+                    {
+                        if (post.Url.ToString().EndsWith(".jpg"))
+                        {
+                            Console.WriteLine(post.Url.ToString());
+                            try
+                            {
+                                client.DownloadFile(post.Url, path + post.Id + ".jpeg");
+                                successfulDownloadCount++;
+                                if (successfulDownloadCount == 10)
+                                {
+                                    break;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Exception Caught.");
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+                    }
+                //}
+                //catch(Exception e)
+                //{
+                //    Console.WriteLine("There is a connection problem.");
+                //    Console.WriteLine(e.Message);
+                //}
+            }
+            else
+            {
+                Console.WriteLine("The subreddit you requested for does not exist.");
+            }
+            //Console.WriteLine(subReddit.Id );
+            Console.WriteLine("fin");
+        }
+    }
+}
